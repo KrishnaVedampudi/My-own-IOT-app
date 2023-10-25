e3ac4c02bc286187e53a8a69374dc972c976db86
Krishna <72448053+KrishnaVedampudi@users.noreply.github.com>
2023-10-25 19:05:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template
 from paho.mqtt import client as mqtt_client
 
 app = Flask(__name__)
+
 broker = "broker.emqx.io"
 port = 1883
 topic = "topicName/pir"
@@ -17,32 +18,29 @@ def connect():
     client = mqtt_client.Client(client_id)
     client.username_pw_set(username, password)
     client.on_connect = on_connect
-    client.on_subscribe = on_subscribe
+    client.on_message = on_message
     client.connect(broker, port)
+    client.subscribe(topic)
     return client
 
-def on_connect(client: mqtt_client, userdata, flags, rc):
+def on_connect(client, userdata, flags, rc):
     if rc == 0:
-        print("Connected to MQTT Broker! Result code: " + str(rc))
+        print("Connected to MQTT Broker!")
         client.subscribe(topic)
     else:
         print("Failed to connect, return code: " + str(rc))
 
-def on_subscribe(client, userdata, mid, granted_qos):
-    print("Subscribed to topic with QoS: " + str(granted_qos))
-
 def on_message(client, userdata, msg):
     global detection
-    detection = msg.payload.decode()
+    detection = int(msg.payload.decode())
     print("Detection:", detection)
 
 @app.route('/')
-def check_detection():
-    client_run = connect()
-    client_run.loop_start()
-    time.sleep(1)
-    status = int(detection)
-    return render_template('index.html', status=status)
+def index():
+    global detection
+    return render_template('index.html', status=detection)
 
 if __name__ == "__main__":
-    app.run()
+    client = connect()
+    client.loop_start()
+    app.run(debug=True)
